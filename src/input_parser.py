cb70cb1c255757988a470e2cc440cd8dedbdf61a
include4eto <traiko.dinev@gmail.com>
2016-10-11 22:26:49 +0100

    Parser update: area_idx and add parseError/Warning lists. No more stout printing here.

--- src/input_parser.py
+++ src/input_parser.py
@@ -1,13 +1,15 @@
 class InputParser:
 	"""Parses an input file and returns a configuration object."""
 
-	NON_EXISTING_ATTRIBUTE_WARNING = "Parse Warning({0}): attribute {1} is not recognized."
+	UNRECOGNIZED_PARAMETER_WARNING = "Parse Warning({0}): parameter {1} is not recognized."
+	MALFORMED_PARAMETER_ERROR = "Parse Error({0}): parameter {1} is malformed."
+	CONVERSION_ERROR = "Parse Error({0}): Wrong type given for value(s) of {1}."
 
 	# Map from name to type, so we know how to parse.
 	# 	<name>: <expected_type>
 	# NOTE: We don't distinguish between integer types,
 	#	uint8 or uint16
-	attribute_map = {
+	_parameters_map = {
 		'lorryVolume': 'int',
 		'lorryMaxLoad': 'int',
 		'binServiceTime': 'float',
@@ -29,13 +31,66 @@ class InputParser:
 		'warmUpTime': 'float'
 	}
 
-
 	def __init__(self, file_name):
 		self.file_name = file_name
 
-	def _roads_layout(self, layout):
+		self.config = {}
+		"""The parsed configuration object"""
+
+		self.parseErrors = []
+		"""The parse errors"""
+
+		self.parseWarnings = []
+		"""The parse warnings"""
+
+	@classmethod
+	def _cast_value(type, value):
+		try:
+			# NOTE to self: there might be a better way to do this
+			if type == 'int':
+				return int(value)
+			if type == 'float':
+				return float(value)
+		except ValueError:
+			return False
+		
+
+	# TODO: maybe remove lineNo, it might not be a good idea
+	#	for it to be passed as an argument
+	def _parse_area_idx(self, lineNo, params, config):
+		"""Parses the area_idx parameter"""
+
+		# parameters should be a list of tuples, as such
+		#	the length should be divisible by 2
+		if length(params) % 2 != 0:
+			self.parseWarnings.append(
+				MALFORMED_PARAMETER_ERROR.format(lineNo, params[0])
+			)
+			return False
+
+		_config = InputParser._parameters_map['areaIdx']
+		# every even element is a key (parameter name)
+		param_names = params[0::2]
+		# every odd element should be a value
+		param_values = [InputParser._cast_value(x) for x in params[1::2]]
+
+		# if any are False, display a conversion Error
+		if (any[val == False for val in param_values])
+			self.parseWarnings.append(
+				CONVERSION_ERROR.format(lineNo, params[0])
+			)
+			return False
+
+		config.area_idx = dict(zip(param_names, param_values))
+
+		return True
+
+	@classmethod
+	def _parse_roads_layout(layout):
 		"""Parses the roads layout. Returns a layout dictionary."""
 		pass
+
+	
 
 	def parse(self):
 		# open the file in read only mode
@@ -54,8 +109,17 @@ class InputParser:
 				continue
 
 			attr_name = attrs[0]
-			# non-existing attributes are ignored with a warning
-			if attr_name not in attribute_map:
-				print(NON_EXISTING_ATTRIBUTE_WARNING.format(idx, attr_name))
+			# non-existing parameters are ignored with a warning
+			if attr_name not in InpuParser._parameters_map:
+				self.parseWarnings.append(NON_EXISTING_PARAMETER_WARNING.format(idx, attr_name))
+
+			# areaIdx and roadsLayout are special cases
+			if attr_name == 'areaIdx':
+				status = InputParser._parse_area_idx(idx, attrs, self.config)
+				if not status:
+					return False
+
+				continue
 			
+			# 
 			
