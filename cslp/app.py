2e874c616249eaec5d462d6b1452e5574152ea04
include4eto <traiko.dinev@gmail.com>
2016-10-15 18:06:32 +0100

    Make simulation.sh read input file and exit, reporting erros, if any

--- cslp/app.py
+++ cslp/app.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 import sys
+from input_parser import *
+
+def print_usage():
+	print('Usage: TODO')
 
 if __name__ == '__main__':
-	pass
+	if len(sys.argv) == 1:
+		print_usage()
+		sys.exit(0)
+	
+	file_path = sys.argv[1]
+	 
+	# create the parser
+	parser = InputParser(file_path)
+	result = parser.parse()
+
+	if not result:
+		print('Parse errors occurred. Exiting...')
+		for i in (parser.parse_errors + parser.parse_warnings):
+			print(i)
+
+		sys.exit(1)
+	
+	print('Configuration read from {0}.'.format(file_path))
+	print('Exiting.')
+	
--- simulate.sh
+++ simulate.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 # This script will be used to launch simulations 
+python3 cslp/app.py "$@"
