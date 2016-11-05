7f585cd2b5b790fd4c294dc35c5cc9043c4591d2
include4eto <traiko.dinev@gmail.com>
2016-11-05 19:44:05 +0000

    Document parser test better and add an expected configuration for the simple test

--- test/input_parser_test.py
+++ test/input_parser_test.py
@@ -8,24 +8,84 @@ class InputParserTest(unittest.TestCase):
 	pp = pprint.PrettyPrinter(indent = 2)
 
 	def test_valid_input(self):
+		# checks that a valid simple input is parsed
 		parser = InputParser('test/inputs/basic_input.txt')
 		result = parser.parse()
+		# this is the expected configuration of the input
+		expected_config = {
+			'experiments': {},
+			'lorryVolume': 20,
+			'lorryMaxLoad': 7000,
+			'binServiceTime': 130,
+			'binVolume': 2,
+			'disposalDistrRate': 2.0,
+			'disposalDistrShape': 2,
+			'bagVolume': 0.05,
+			'bagWeightMin': 2,
+			'bagWeightMax': 8,
+			'stopTime': 16.0,
+			'warmUpTime': 12.0,
+			'noAreas': 1,
+			'areas': [
+				{
+					'noBins': 5,
+					'serviceFreq': 0.0625,
+					'thresholdVal': 0.75,
+					'areaIdx': 0,
+					'roadsLayout': [
+						[
+							{ 'index': 0, 'path_length': 0 },
+						 	{ 'index': 1, 'path_length': 3 },
+						 	{ 'index': 5, 'path_length': 4 }
+						],
+						[
+							{ 'index': 0, 'path_length': 3 },
+							{ 'index': 1, 'path_length': 0 },
+							{ 'index': 2, 'path_length': 5 }
+						],
+						[
+							{ 'index': 2, 'path_length': 0 },
+							{ 'index': 3, 'path_length': 2 },	
+						],
+						[
+							{ 'index': 3, 'path_length': 0 },
+							{ 'index': 4, 'path_length': 2 },
+							{ 'index': 5, 'path_length': 2 },	
+						],
+						[
+							{ 'index': 1, 'path_length': 1 },
+							{ 'index': 4, 'path_length': 0 }
+						],
+						[
+							{ 'index': 0, 'path_length': 4 },
+							{ 'index': 3, 'path_length': 2 },
+							{ 'index': 4, 'path_length': 4 },
+							{ 'index': 5, 'path_length': 0 },
+						]
+					]
+				}
+			]
+		}
 
+		self.assertEqual(expected_config, parser.config)
 		self.assertTrue(result)
 	
 	def test_invalid_input(self):
+		# check against an invalid simple input
 		parser = InputParser('test/inputs/invalid.in.txt')
 		result = parser.parse()
 
 		self.assertFalse(result)
 
 	def test_grid_input(self):
+		# a grid input with big areas, still valid
 		parser = InputParser('test/inputs/grids.dat.txt')
 		result = parser.parse()
 
 		self.assertTrue(result)
 
 	def test_valid_many(self):
+		# a test input with many valid areas
 		parser = InputParser('test/inputs/valid_many_areas.txt')
 		result = parser.parse()
 		pprint.pprint(parser.parse_errors)
@@ -33,16 +93,15 @@ class InputParserTest(unittest.TestCase):
 		self.assertTrue(result)
 
 	def test_invalid_rads(self):
+		# a test input with valid parameters, but invalid roads layout
 		parser = InputParser('test/inputs/invalid_roads_layout.txt')
 		result = parser.parse()
 
-		# print(parser.parse_errors, parser.parse_warnings)
 		self.assertFalse(result)
 
 	def test_experimentation(self):
+		# a valid experimentation input
 		parser = InputParser('test/inputs/experiment_input.txt')
 		result = parser.parse()
 		
 		self.assertTrue(result)
-
-	# def test_valid_simple(self):
