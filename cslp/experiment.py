d4a051da348e8f3f2206dd206accacce104ea592
include4eto <traiko.dinev@gmail.com>
2016-12-16 21:53:50 +0000

    Experimentation basic support

---
+++ cslp/experiment.py
@@ -0,0 +1,85 @@
+from simulation.event_dispatcher import EventDispatcher
+from simulation.simulation import Simulation
+from output_formatter import OutputFormatter
+from config import config as app_config
+from statistics.statistics_aggregator import StatisticsAggregator
+import itertools
+
+import pprint
+
+class Experiment:
+	EXPERIMENT_HEADER = "Experiment #{0}: {1}"
+
+	def __init__(self, config):
+		self.config = config
+
+		# make a grid of all experimentation configurations
+		if len(config['experiments']):
+			# convert to tuples so we can create the cartesian product
+			experiments = []
+			for k, values in config['experiments'].items():
+				experiment = []
+				for v in values:
+					experiment.append({ 'key': k, 'value': v})
+				experiments.append(experiment)
+
+			# get the cartesian product
+			self.grid = itertools.product(*experiments)
+		else:
+			self.grid = None
+
+	def run_all(self):
+		if self.grid is None:
+			# this means no experiments, enable verbose output and run the one simulation
+			self._run_experiment(self.config, enable_verbose = True)
+		else:
+			count = 1
+			# run all experimentations
+			for experiment in self.grid:
+				description = ""
+				for var in experiment:
+					self.config[var['key']] = var['value']
+					description += ' {0} {1}'.format(var['key'], var['value'])
+				
+				experiment_text = Experiment.EXPERIMENT_HEADER.format(
+					count, description
+				)
+				print(experiment_text)
+				count += 1
+				# run the experiment
+				self._run_experiment(self.config, enable_verbose = False)
+
+
+	def _run_experiment(self, config, enable_verbose = False):
+
+		# convert stop time to seconds
+		stop_time = config['stopTime'] * 60 * 60
+		stop_time = int(stop_time)
+
+		# first create an event dispatcher
+		dispatcher = EventDispatcher(stop_time, config['noAreas'])
+
+		sim = Simulation(config, dispatcher)
+		# the simulation checks for valid configuration, see if there were any errors
+		for i in (sim.validate_errors + sim.validate_warnings):
+			print(i)
+		if sim.simulation_aborted:
+			print('Configuration validation errors occurred. Exiting...\n')
+			sys.exit(0)
+			return
+		
+
+		# create the output formatter, if we need verbose output
+		if enable_verbose:
+			output_formatter = OutputFormatter(dispatcher)
+		
+		statistics_aggregator = StatisticsAggregator(config, dispatcher)
+		sim.run()
+
+		while True:
+			current_time = dispatcher.next_event()
+
+			if current_time == False:
+				# simulation end
+				break
+		statistics_aggregator.print_output()
---
+++ test/statistics_aggregator_test.py
@@ -0,0 +1,96 @@
+import unittest
+import sys
+from cslp.statistics.statistics_aggregator import StatisticsAggregator
+from cslp.simulation.event_dispatcher import Event, EventDispatcher
+from StringIO import StringIO
+
+class StatisticsAggregatorTest(unittest.TestCase):
+	"""Tests the aggregated statistics class"""
+
+	config = {
+		'noAreas': 2,
+		'warmUpTime': 0.00001,
+		'areas': [
+			{
+				'noBins': 3
+			},
+			{
+				'noBins': 2
+			}
+		]
+	}
+
+	def test_trip_duration(self):
+		dispatcher = EventDispatcher(100, 2)
+		statistics_aggregator = StatisticsAggregator(StatisticsAggregatorTest.config, dispatcher)
+
+		dispatcher.events = [
+			# disposal at 1, no overflows
+			Event(time = 1, area_index = 0, type = 'service_time'),
+			
+			# lower the total service time
+			Event(time = 1, area_index = 1, type = 'service_time'),
+
+			Event(time = 1, area_index = 0, type = 'bins_overflowed_at_service_time', data = { 'no_bins': 1 }),
+			Event(time = 1, area_index = 0, type = 'lorry_departure', data = { 'lorry_idx': 0, 'location': 0 }),
+			
+			# arrival at bin 1
+			Event(time = 2, area_index = 0, type = 'lorry_arrival', data = { 'lorry_idx': 0, 'location': 1, 'distance_travelled': 5 }),
+			
+			# empty the bin after 1 second
+			Event(time = 3, area_index = 0, type = 'lorry_load_changed', data = {
+				'lorry_id': 0,
+				'bin_idx': 0,
+				'lorry_volume': 5,
+				'lorry_weight': 6
+			}),
+
+			# depart from the bin
+			Event(time = 3, area_index = 0, type = 'lorry_departure', data = { 'lorry_idx': 0, 'location': 1 }),
+
+			# arrive at 0
+			Event(time = 4, area_index = 0, type = 'lorry_arrival', data = { 'lorry_idx': 0, 'location': 0, 'distance_travelled': 5 }),
+			Event(time = 4, area_index = 0, type = 'lorry_load_changed', data = {
+				'lorry_id': 0,
+				'bin_idx': 0,
+				'lorry_volume': 5,
+				'lorry_weight': 6
+			}),
+		]
+
+		for i in xrange(9):
+			time = dispatcher.next_event()
+
+		sys_stdout = sys.stdout
+
+		captured_out = StringIO()
+		sys.stdout = captured_out
+		statistics_aggregator.print_output()
+		
+		self.assertEqual(captured_out.getvalue(),
+			"---\n" +
+			"area 0: average trip duration 00:03\n" +
+			"area 1: average trip duration 00:00\n" +
+			# we have 2 areas, total average 1.5
+			"overall average trip duration 00:03\n" +
+
+			"area 0: average no. trips 1.000\n" +
+			"area 1: average no. trips 0.000\n" +
+			"overall average no. trips 0.500\n" +
+			
+			"area 0: trip efficiency 2.000\n" +
+			"area 1: trip efficiency 0.000\n" +
+			"overall trip efficiency 2.000\n" +
+			
+			"area 0: average volume collected 5.000\n" +
+			"area 1: average volume collected 0.000\n" +
+			"overall average volume collected 5.000\n" +
+			
+			"area 0: percentage of bins overflowed 33\n" +
+			"area 1: percentage of bins overflowed 0\n" +
+			"overall percentage of bins overflowed 16\n" +
+			
+			"---\n"
+		)
+		sys.stdout = sys_stdout
+	# def test_
