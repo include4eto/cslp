07a36c39bbf903c13124a00024f05c99fe0a5390
include4eto <traiko.dinev@gmail.com>
2016-12-17 10:44:42 +0000

    Add sample performance tests

---
+++ test/generate_performance_test.py
@@ -0,0 +1,20 @@
+# Generates a valid big area to test
+#	the performance of the simulation
+
+import random
+import sys
+
+no_bins = 65535
+no_bins = 300
+
+for y in range(no_bins):
+	r = []
+	for x in range(no_bins):
+		if x == y:
+			sys.stdout.write(' 0')
+		else:
+			path_len = 10
+			sys.stdout.write(' {0}'.format(path_len))
+	sys.stdout.write('\n')
+		
+sys.stdout.flush()
