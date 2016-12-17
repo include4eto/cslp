e2f5cc5027199224fd88c771488fb9bc955116a0
include4eto <traiko.dinev@gmail.com>
2016-12-17 10:42:18 +0000

    Rearrange input test files

--- test/input_parser_test.py
+++ test/input_parser_test.py
@@ -10,7 +10,7 @@ class InputParserTest(unittest.TestCase):
 
 	def test_valid_input(self):
 		# checks that a valid simple input is parsed
-		parser = InputParser('test/inputs/basic_input.txt')
+		parser = InputParser('test/inputs/parser_tests/basic_input.txt')
 		result = parser.parse()
 		# this is the expected configuration of the input
 		expected_config = {
@@ -73,21 +73,21 @@ class InputParserTest(unittest.TestCase):
 	
 	def test_invalid_input(self):
 		# check against an invalid simple input
-		parser = InputParser('test/inputs/invalid.in.txt')
+		parser = InputParser('test/inputs/parser_tests/invalid.in.txt')
 		result = parser.parse()
 
 		self.assertFalse(result)
 
 	def test_grid_input(self):
 		# a grid input with big areas, still valid
-		parser = InputParser('test/inputs/grids.dat.txt')
+		parser = InputParser('test/inputs/parser_tests/grids.dat.txt')
 		result = parser.parse()
 
 		self.assertTrue(result)
 
 	def test_valid_many(self):
 		# a test input with many valid areas
-		parser = InputParser('test/inputs/valid_many_areas.txt')
+		parser = InputParser('test/inputs/parser_tests/valid_many_areas.txt')
 		result = parser.parse()
 		pprint.pprint(parser.parse_errors)
 
@@ -95,14 +95,14 @@ class InputParserTest(unittest.TestCase):
 
 	def test_invalid_roads(self):
 		# a test input with valid parameters, but invalid roads layout
-		parser = InputParser('test/inputs/invalid_roads_layout.txt')
+		parser = InputParser('test/inputs/parser_tests/invalid_roads_layout.txt')
 		result = parser.parse()
 
 		self.assertFalse(result)
 
 	def test_experimentation(self):
 		# a valid experimentation input
-		parser = InputParser('test/inputs/experiment_input.txt')
+		parser = InputParser('test/inputs/parser_tests/experiment_input.txt')
 		result = parser.parse()
 		
 		self.assertTrue(result)
@@ -110,7 +110,7 @@ class InputParserTest(unittest.TestCase):
 	def test_magnitude_violation(self):
 		# tests a validly formatted input, but
 		# 	with wrong ranges
-		parser = InputParser('test/inputs/invalid_magnitude.txt')
+		parser = InputParser('test/inputs/parser_tests/invalid_magnitude.txt')
 		result = parser.parse()
 
 		self.assertFalse(result)
@@ -118,7 +118,7 @@ class InputParserTest(unittest.TestCase):
 	def test_extra_parameters(self):
 		# test that extra paratemeters are not allowed
 
-		parser = InputParser('test/inputs/extra_parameters.txt')
+		parser = InputParser('test/inputs/parser_tests/extra_parameters.txt')
 		result = parser.parse()
 
 		self.assertFalse(result)
@@ -126,7 +126,7 @@ class InputParserTest(unittest.TestCase):
 	def test_invalid_matrix(self):
 		# test that all paths should be greater than 0 apart from the diagonal
 
-		parser = InputParser('test/inputs/test_invalid_matrix.txt')
+		parser = InputParser('test/inputs/parser_tests/test_invalid_matrix.txt')
 		result = parser.parse()
 
 		self.assertFalse(result)
@@ -134,7 +134,7 @@ class InputParserTest(unittest.TestCase):
 	
 	def test_max_magnitude(self):
 		# test that the maximum magnitudes are allowed
-		parser = InputParser('test/inputs/max_magnitudes.txt')
+		parser = InputParser('test/inputs/parser_tests/max_magnitudes.txt')
 		result = parser.parse()
 
 		self.assertTrue(result)
@@ -142,7 +142,7 @@ class InputParserTest(unittest.TestCase):
 	def test_whitespaces(self):
 		# test that more than one whitespaces are stripped
 		#	this includes tabs, but not new lines 
-		parser = InputParser('test/inputs/whitespaces.txt')
+		parser = InputParser('test/inputs/parser_tests/whitespaces.txt')
 		result = parser.parse()
 
 		self.assertTrue(result)
